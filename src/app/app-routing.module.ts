724840fc94498b866c78bcd04b663f465bb59210
k7494@office365svip.tw <k7494@office365svip.tw>
2021-02-20 20:16:55 +0530

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StaticComponent } from './staticcontent/static.component';
 import { DynamicapiComponent } from './dynamicontent/dynamicapi.component';
-import { HomeComponent } from './home/home.component';
+import { ContentComponent } from './content/content.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'content', pathMatch: 'full' },
+  { path: 'content', component: ContentComponent },
   { path: 'static', component: StaticComponent },
   { path: 'dynamic', component: DynamicapiComponent }
 ];
